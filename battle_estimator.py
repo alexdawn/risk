2ef876f69dd6cf4e971cf7deb9ad5db517292fb0
Alex Dawn <al_4242@hotmail.co.uk>
2020-04-22 21:32:35 +0100

    Create transition matrix

--- battle_estimator.py
+++ battle_estimator.py
@@ -1,5 +1,10 @@
+import numpy as np
 from typing import Callable, Any
-from itertools import product
+from itertools import product, chain
+from functools import lru_cache
+
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import inv
 
 
 def probY(y1: int, y2: int = None) -> float:
@@ -46,12 +51,13 @@ def dice(dice: int) -> Callable[[Any, Any], float]:
     return functions[dice]
 
 
+@lru_cache()
 def probable_outcome(
         attackers: int, defenders: int, defender_loses: int) -> float:
     """Probability P(i,j,k) of Defender losing k given i attackers and j defenders"""
-    assert attackers >= 1 and attackers <= 4
-    assert defenders >= 1 and defenders <= 2
-    assert defender_loses >= 0 and defender_loses <= 2
+    assert attackers >= 1 and attackers <= 4, "Invalid attackers {}".format(attackers)
+    assert defenders >= 1 and defenders <= 2, "Invalid defenders {}".format(defenders)
+    assert defender_loses >= 0 and defender_loses <= 2, "Invalid losers {}".format(defender_loses)
     Attacker = dice(attackers)
     Defender = dice(defenders)
     die = range(1, 7)
@@ -75,3 +81,38 @@ def probable_outcome(
                     (y1 <= z1 and y2 <= z2 and defender_loses == 0)):
                 prob += Attacker(y1, y2) * Defender(z1, z2)
     return prob
+
+
+def probability_win(A, D):
+    # Generate possible states
+    transient_state = [
+        (a, d) for a, d in product(range(1, A + 1), range(1, D + 1))
+    ]
+    absorbing_state = [
+        (a, d) for a, d in chain(zip([0] * D, range(1, D + 1)), zip(range(1, A + 1), [0] * A))
+    ]
+    states = transient_state + absorbing_state
+    state_lookup = {s: i for i, s in enumerate(states)}
+    state_size = len(states)
+
+    # Add probability to transition elements
+    row = []
+    col = []
+    data = []
+    for i, (a, d) in enumerate(states):
+        if a > 0 and d > 0:
+            max_deaths = 2 if a > 1 and d > 1 else 1
+            for dl in range(0, max_deaths + 1):
+                al = max_deaths - dl
+                row.append(i)
+                col.append(state_lookup[(a - al, d - dl)])
+                data.append(probable_outcome(min(a, 3), min(d, 2), dl))
+        else:
+            row.append(i)
+            col.append(i)
+            data.append(1)
+    A = csr_matrix((data, (row, col)), shape=(state_size, state_size))
+    print(A)
+
+
+probability_win(30, 30)
